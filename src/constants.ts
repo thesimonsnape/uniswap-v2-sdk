1c1d8b6116ef194ce7605867b190773cfa71b645
Simon Snape <simonsnape@proton.me>
2024-02-07 20:45:34 +0530

    feat: update addresses

--- src/constants.ts
+++ src/constants.ts
@@ -22,9 +22,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xf0cd19546801D467182b65a358b36FC8af91C585'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0xed2683bad1f2d8ce1ab0d6db1892520df2fb0b2244efcd26d742f7b021e929fb'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
